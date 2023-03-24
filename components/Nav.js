cc08ddbb412b2686c0ae4bce9b4e60e849dec26f
l3x3d <admin@falsezero.io>
2023-03-23 18:04:45 -0700

    modified:   components/Footer.js 	modified:   components/Hero.js 	modified:   components/Nav.js 	modified:   pages/index.js

--- components/Nav.js
+++ components/Nav.js
@@ -17,7 +17,7 @@ export default function Nav() {
         <Link href="/" passHref>
           <a className="cursor-pointer">
             <span className="text-lg pt-1 font-bold">
-              Shopify + Next.js
+              /components/nav.js
             </span>
           </a>
         </Link>
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ products }) {
   return (
     <div className="">
       <Head>
-        <title>Modern eCommerce Course</title>
+        <title>NEXTSHOP_SSRBASE</title>
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta httpEquiv="Content-Type" content="text/html; charset=ISO-8859-1" />
         <meta name="description" content="Modern eCommerce Development Course focusing on Shopify, Next.js, TailwindCSS, GraphQL. Additional topics include Storefront API, Static Site Generation, getStaticPaths, getStaticProps and more." />
