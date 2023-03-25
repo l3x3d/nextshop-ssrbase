3b0ad7e2fdb3ed6d2162d194f11cf4c93e49e125
l3x3d <admin@falsezero.io>
2023-03-24 23:10:31 -0700

    modified:   components/Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -4,12 +4,12 @@ export default function Hero() {
   return (
     <div className="my-48 mx-auto max-w-7xl px-4 sm:mt-24 md:mt-72 text-center">
       <h1 className="font-extrabold text-gray-900">
-        <p className="text-xl sm:text-3xl md:text-4xl">Prebuilt for Shopify + Next.js + Tailwind</p>
-        <p className="text-transparent bg-clip-text bg-gradient-to-r from-blue-900 via-indigo-500 to-gray-900 text-4xl sm:text-6xl md:text-7xl">Falsezero SSR <br></br>commerce starter
+        <p className="text-xl sm:text-3xl md:text-4xl">/////////////////</p>
+        <p className="text-transparent bg-clip-text bg-gradient-to-r from-blue-900 via-indigo-500 to-gray-900 text-4xl sm:text-6xl md:text-7xl">falsezero headless boilerplate
         </p>
       </h1>
       <h2 className="mt-3 max-w-md mx-auto text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-x-3xl">
-        Edit this page from /components/hero.js
+        /components/hero.js
       </h2>
       <div className="mt-5 max-w-md mx-auto flex justify-center items-center md:mt-8">
         <a href="https://falsezero.io" className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium py-3 border-transparent rounded-md text-white bg-gray-900 hover:bg-gray-800">
